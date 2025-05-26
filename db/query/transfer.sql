92c7270b70e1dcf39d82f0767469236e2ec946e1
kuluruvineeth <kuluruvineeth8623@gmail.com>
2025-05-26 10:55:41 +0530

    fix: change OR to AND in ListTransfers query logic

--- db/query/transfer.sql
+++ db/query/transfer.sql
@@ -14,7 +14,7 @@ WHERE id = $1 LIMIT 1;
 
 -- name: ListTransfers :many
 SELECT * FROM transfers
-WHERE from_account_id = $1 OR to_account_id = $2
+WHERE from_account_id = $1 AND to_account_id = $2
 ORDER BY id
 LIMIT $3
 OFFSET $4;
