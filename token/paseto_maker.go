47a6976dd13c621fcc819ea89039ae96a3120643
kuluruvineeth <kuluruvineeth8623@gmail.com>
2025-05-27 12:45:43 +0530

    refactor: update PASETO maker to use shared payload structure

--- token/paseto_maker.go
+++ token/paseto_maker.go
@@ -5,7 +5,6 @@ import (
 	"time"
 
 	"github.com/aead/chacha20poly1305"
-	"github.com/google/uuid"
 	"github.com/o1egl/paseto"
 )
 
@@ -16,7 +15,7 @@ type PasetoMaker struct {
 
 func NewPasetoMaker(symmetricKey string) (Maker, error) {
 	if len(symmetricKey) != chacha20poly1305.KeySize {
-		return nil, fmt.Errorf("invalid key size: must be %d characters", chacha20poly1305.KeySize)
+		return nil, fmt.Errorf("invalid key size: must be exactly %d characters", chacha20poly1305.KeySize)
 	}
 
 	paseto := paseto.NewV2()
@@ -29,32 +28,26 @@ func NewPasetoMaker(symmetricKey string) (Maker, error) {
 }
 
 func (maker *PasetoMaker) CreateToken(username string, duration time.Duration) (string, error) {
-	tokenID, err := uuid.NewRandom()
+	payload, err := NewPayload(username, duration)
 	if err != nil {
 		return "", err
-	}
-
-	payload := Payload{
-		ID:        tokenID,
-		Username:  username,
-		IssuedAt:  time.Now(),
-		ExpiredAt: time.Now().Add(duration),
 	}
 
 	return maker.paseto.Encrypt(maker.symmetricKey, payload, nil)
 
 }
 
-func (maker *PasetoMaker) VerifyToken(tokenString string) (*Payload, error) {
+func (maker *PasetoMaker) VerifyToken(token string) (*Payload, error) {
 	payload := &Payload{}
 
-	err := maker.paseto.Decrypt(tokenString, maker.symmetricKey, payload, nil)
+	err := maker.paseto.Decrypt(token, maker.symmetricKey, payload, nil)
 	if err != nil {
 		return nil, ErrInvalidToken
 	}
 
-	if time.Now().After(payload.ExpiredAt) {
-		return nil, ErrExpiredToken
+	err = payload.Valid()
+	if err != nil {
+		return nil, err
 	}
 
 	return payload, nil
