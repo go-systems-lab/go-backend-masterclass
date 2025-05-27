ec4aebe57eb6d2836dd3d0b9a42e1e5bfe505b9d
kuluruvineeth <kuluruvineeth8623@gmail.com>
2025-05-27 08:31:53 +0530

    refactor: extract currency validation to utility package

--- api/validator.go
+++ api/validator.go
@@ -1,17 +1,13 @@
 package api
 
-import "github.com/go-playground/validator/v10"
-
-var supportedCurrencies = map[string]bool{
-	"USD": true,
-	"EUR": true,
-	"CAD": true,
-	"INR": true,
-}
+import (
+	"github.com/go-playground/validator/v10"
+	"github.com/go-systems-lab/go-backend-masterclass/util"
+)
 
 var validCurrency validator.Func = func(fieldLevel validator.FieldLevel) bool {
 	if currency, ok := fieldLevel.Field().Interface().(string); ok {
-		return supportedCurrencies[currency]
+		return util.IsSupportedCurrency(currency)
 	}
 	return false
 }
